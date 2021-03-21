aef0237a77bf95f33061b3b1623235ef506898c8
DegrangeM <53106394+DegrangeM@users.noreply.github.com>
2021-03-21 02:50:06 +0100

    Add set button shortcut

--- h5p-shortcut-selector.js
+++ h5p-shortcut-selector.js
@@ -33,54 +33,69 @@ H5PEditor.widgets.shortcutSelector = H5PEditor.shortcutSelector = (function ($) 
     this.$item = $(this.createHtml()).appendTo($wrapper);
     this.$item.addClass('h5p-shortcut-selector');
     this.$inputs = this.$item.find('input');
+
+    this.$item.find('button').click(function () {
+      that.$inputs.eq(0).val('').data('register-shortcut', true).prop('disabled', false).focus();
+      that.$inputs.eq(1).val('').prop('disabled', false);
+    });
+
     this.$inputs.eq(0).keydown(function (e) {
       // The input will reset when we focus the input, but only after the user start pressing a first key
       // This allow the user to focus the field then do nothing and focus out without resetting the field
       // This is usefull as the user can focus the field by clicking on the label, pressing tab, etc.
-      if ($(this).data('need-reset')) {
-        $(this).val('');
-      }
-      $(this).data('need-reset', false);
+      if ($(this).data('register-shortcut')) {
 
-      const key = H5PEditor.findField('shortcutMode', that.parent).value === 'content' ? e.key : e.code;
-      const keyText = C.getKeyText(key);
+        const key = H5PEditor.findField('shortcutMode', that.parent).value === 'content' ? e.key : e.code;
+        const keyText = C.getKeyText(key);
 
-      // Update the shortcut input with the new key
-      if ($(this).val()) {
-        // This is not the first key of the shortcut, add it to the list
-        if ($(this).val().split('+').indexOf(key) === -1) { // ignore automatic repeat keydown
-          $(this).val($(this).val() + '+' + key);
-          that.$inputs.eq(1).val(that.$inputs.eq(1).val() + '+' + keyText);
-        }
-      }
-      else {
-        // This is the first key of the shortcut
-        $(this).val(key);
-        that.$inputs.eq(1).val(keyText);
-      }
-
-      C.saveChange(that);
-      e.preventDefault();
-    }).focus(function () {
-      $(this).data('need-reset', true);
-    }).blur(function () {
-      // Some shortcut like Alt + Tab can't be detected as they focus out the windows, we can however detect this blur to try to detect the shortcut
-      // The user focus out the input, we need to know if this is just the input or the whole window
-      if (!document.hasFocus()) {
-        // The whole windows have been unfocused
-        $(this).val($(this).val() + '+blur');
-        that.$inputs.eq(1).val(that.$inputs.eq(1).val() + '+?');
-        C.saveChange(that);
-      } else { // check again in 100ms
-        setTimeout(function () {
-          if (!document.hasFocus()) {
-            that.$inputs.eq(0).val(that.$inputs.eq(0).val() + '+blur');
-            that.$inputs.eq(1).val(that.$inputs.eq(1).val() + '+?');
-            C.saveChange(that);
+        // Update the shortcut input with the new key
+        if ($(this).val()) {
+          // This is not the first key of the shortcut, add it to the list
+          if ($(this).val().split('+').indexOf(key) === -1) { // ignore automatic repeat keydown
+            $(this).val($(this).val() + '+' + key);
+            that.$inputs.eq(1).val(that.$inputs.eq(1).val() + '+' + keyText);
           }
-        }, 100);
+        }
+        else {
+          // This is the first key of the shortcut
+          $(this).val(key);
+          that.$inputs.eq(1).val(keyText);
+        }
+
+        C.saveChange(that);
+        e.preventDefault();
+
+      } else {
+        // TODO ?
       }
-    });
+
+
+    }).blur(function () {
+
+      if ($(this).data('register-shortcut')) {
+        
+        $(this).data('register-shortcut', false).prop('disabled', true);
+
+        // Some shortcut like Alt + Tab can't be detected as they focus out the windows, we can however detect this blur to try to detect the shortcut
+        // The user focus out the input, we need to know if this is just the input or the whole window
+        if (!document.hasFocus()) {
+          // The whole windows have been unfocused
+          $(this).val($(this).val() + '+blur');
+          that.$inputs.eq(1).val(that.$inputs.eq(1).val() + '+?');
+          C.saveChange(that);
+        }
+        else { // check again in 100ms
+          setTimeout(function () {
+            if (!document.hasFocus()) {
+              that.$inputs.eq(0).val(that.$inputs.eq(0).val() + '+blur');
+              that.$inputs.eq(1).val(that.$inputs.eq(1).val() + '+?');
+              C.saveChange(that);
+            }
+          }, 100);
+        }
+
+      }
+    }).prop('disabled', true);
     this.$inputs.eq(1).blur(function () {
       // The user leaved the alternate text input, we need to check if it's content is correct (i.e. have the good + amount)
 
@@ -97,7 +112,7 @@ H5PEditor.widgets.shortcutSelector = H5PEditor.shortcutSelector = (function ($) 
         $(this).val(that.$inputs.eq(0).val());
       }
       C.saveChange(that);
-    });
+    }).prop('disabled', true);
 
     this.$errors = this.$item.children('.h5p-errors');
 
@@ -109,9 +124,9 @@ H5PEditor.widgets.shortcutSelector = H5PEditor.shortcutSelector = (function ($) 
   C.prototype.createHtml = function () {
     const id = H5PEditor.getNextFieldId(this.field);
     const descriptionId = (this.field.description !== undefined ? H5PEditor.getDescriptionId(id) : undefined);
-    const shortcutInput = H5PEditor.createText(this.params !== undefined ? this.params.keys : undefined, undefined, C.t('clickToSet'), id, descriptionId);
+    const shortcutInput = H5PEditor.createText(this.params !== undefined ? this.params.keys : undefined, undefined, 'Shortcut' /* TODO C.t('clickToSet') */, id, descriptionId);
     const shortcutTextInput = H5PEditor.createText(this.params !== undefined ? this.params.keysText : undefined, undefined, C.t('alternativeText'), undefined, descriptionId);
-    const input = shortcutInput + shortcutTextInput;
+    const input = '<div class="h5p-shortcut-selector-shortcut"><button type="button" class="h5peditor-button-textual">Set shortcut</button>' /* TODO */ + shortcutInput + shortcutTextInput + '</div>';
     return H5PEditor.createFieldMarkup(this.field, input, id);
   };
 
